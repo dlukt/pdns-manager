d101216dc00a962717e8eab27b8a67beea0be917
Darko Luketic <info@icod.de>
2025-08-23 21:13:13 +0200

    prefer env vars over args

--- cmd/start.go
+++ cmd/start.go
@@ -26,15 +26,39 @@ var startCmd = &cobra.Command{
 	Use:   "start",
 	Short: "starts the server",
 	Run: func(cmd *cobra.Command, args []string) {
-		dsn := os.Getenv("DSN")
-		if dsn == "" {
+		var dsn string
+		if config.DSN == "" {
+			dsn = os.Getenv("DSN")
+		} else {
 			dsn = config.DSN
 		}
-		client := openDatabaseConnection(config.DSN)
+		var smtpAddr, smtpUser, smtpPass, mailFrom string
+		if config.SMTPAddr == "" {
+			smtpAddr = os.Getenv("SMTP_ADDR")
+		} else {
+			smtpAddr = config.SMTPAddr
+		}
+		if config.SMTPUser == "" {
+			smtpUser = os.Getenv("SMTP_USER")
+		} else {
+			smtpUser = config.SMTPUser
+		}
+		if config.SMTPPass == "" {
+			smtpPass = os.Getenv("SMTP_PASS")
+		} else {
+			smtpPass = config.SMTPPass
+		}
+		if config.MailFrom == "" {
+			mailFrom = os.Getenv("MAIL_FROM")
+		} else {
+			mailFrom = config.MailFrom
+		}
+
+		client := openDatabaseConnection(dsn)
 		defer client.Close()
 		var mailer auth.Mailer = auth.NewLogMailer()
 		if config.SMTPAddr != "" && config.MailFrom != "" {
-			mailer = auth.NewSMTPMailer(config.SMTPAddr, config.SMTPUser, config.SMTPPass, config.MailFrom)
+			mailer = auth.NewSMTPMailer(smtpAddr, smtpUser, smtpPass, mailFrom)
 		}
 		mux := web.NewHandler(auth.NewService(client, mailer))
 		fmt.Println("listening on :8080")
